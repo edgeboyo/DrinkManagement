95695a297df6e39152570c893c7bb0755721d257
edgeboyo <mkolybko@gmail.com>
2021-10-11 22:51:29 +0100

    Initial bot commit

--- bot.ts
+++ bot.ts
@@ -1,3 +1,21 @@
-const helloMessage: string = "HelloWorld";
+import * as dotenv from "dotenv";
+import { Client, Intents } from "discord.js";
 
-console.log(helloMessage);
+dotenv.config();
+
+//Check if a discord token is provided
+if (process.env.DISCORD_TOKEN === undefined) {
+  console.log("No DISCORD_TOKEN provided in .env file");
+  process.exit(1);
+}
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
